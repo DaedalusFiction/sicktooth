c25d70795983196218d1cab3827200a7b3f88c02
David Sorensen <teardownthismall@gmail.com>
2022-03-17 13:48:01 -0400

    added authentication to admin

--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -57,6 +57,7 @@ const Admin = () => {
 
     const testbutton = () => {
         console.log(currentUser.email);
+        console.log(currentUser.uid);
     };
 
     const upload = async () => {
@@ -116,79 +117,100 @@ const Admin = () => {
             <Button onClick={signIn} color="secondary">
                 Sign In
             </Button>
-            <Button onClick={testbutton} color="secondary">
+            {/* <Button onClick={testbutton} color="secondary">
                 test
-            </Button>
-            <Paper color="seconary" sx={{ padding: "1em" }}>
-                <Box
-                    sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        gap: "1em",
-                    }}
-                >
-                    <TextField id="title" label="Title" variant="outlined" />
-                    <TextField id="author" label="Author" variant="outlined" />
-                    <TextField
-                        id="bio"
-                        label="Bio"
-                        multiline
-                        value={bio}
-                        onChange={handleBioChange}
-                        minRows={4}
-                        variant="outlined"
-                    />
-                    <FormControl>
-                        <InputLabel className="select-item" id="genre-label">
-                            Genre
-                        </InputLabel>
-                        <Select
-                            className="select-item"
-                            labelId="genre-label"
-                            id="genre"
-                            value={genre}
-                            label="Genre"
-                            onChange={handleGenreChange}
-                        >
-                            <MenuItem className="select-item" value="Fiction">
-                                Fiction
-                            </MenuItem>
-                            <MenuItem
+            </Button> */}
+            {currentUser && currentUser.uid === "6OpS62AAUzcxy2z3IaHtQWdkiel1" && (
+                <Paper color="seconary" sx={{ padding: "1em" }}>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            gap: "1em",
+                        }}
+                    >
+                        <TextField
+                            id="title"
+                            label="Title"
+                            variant="outlined"
+                        />
+                        <TextField
+                            id="author"
+                            label="Author"
+                            variant="outlined"
+                        />
+                        <TextField
+                            id="bio"
+                            label="Bio"
+                            multiline
+                            value={bio}
+                            onChange={handleBioChange}
+                            minRows={4}
+                            variant="outlined"
+                        />
+                        <FormControl>
+                            <InputLabel
                                 className="select-item"
-                                value="Non-Fiction"
+                                id="genre-label"
                             >
-                                Non-Fiction
-                            </MenuItem>
-                            <MenuItem className="select-item" value="Poetry">
-                                Poetry
-                            </MenuItem>
-                        </Select>
-                    </FormControl>
-                    <LocalizationProvider dateAdapter={AdapterDateFns}>
-                        <DatePicker
-                            label="Date of Publication"
-                            value={date}
-                            onChange={(newValue) => {
-                                setDate(format(newValue, "MMMM d, yyyy"));
-                            }}
-                            renderInput={(params) => <TextField {...params} />}
-                        />
-                    </LocalizationProvider>
-                    <Button variant="contained" component="label">
-                        Select File
-                        <input
-                            id="storyFile"
-                            type="file"
-                            accept=".md"
-                            hidden
-                            onChange={handleFileInputChange}
-                        />
-                    </Button>
-                    <Button variant="contained" onClick={upload}>
-                        Upload
-                    </Button>
-                </Box>
-            </Paper>
+                                Genre
+                            </InputLabel>
+                            <Select
+                                className="select-item"
+                                labelId="genre-label"
+                                id="genre"
+                                value={genre}
+                                label="Genre"
+                                onChange={handleGenreChange}
+                            >
+                                <MenuItem
+                                    className="select-item"
+                                    value="Fiction"
+                                >
+                                    Fiction
+                                </MenuItem>
+                                <MenuItem
+                                    className="select-item"
+                                    value="Non-Fiction"
+                                >
+                                    Non-Fiction
+                                </MenuItem>
+                                <MenuItem
+                                    className="select-item"
+                                    value="Poetry"
+                                >
+                                    Poetry
+                                </MenuItem>
+                            </Select>
+                        </FormControl>
+                        <LocalizationProvider dateAdapter={AdapterDateFns}>
+                            <DatePicker
+                                label="Date of Publication"
+                                value={date}
+                                onChange={(newValue) => {
+                                    setDate(format(newValue, "MMMM d, yyyy"));
+                                }}
+                                renderInput={(params) => (
+                                    <TextField {...params} />
+                                )}
+                            />
+                        </LocalizationProvider>
+                        <Button variant="contained" component="label">
+                            Select File
+                            <input
+                                id="storyFile"
+                                type="file"
+                                accept=".md"
+                                hidden
+                                onChange={handleFileInputChange}
+                            />
+                        </Button>
+                        <Button variant="contained" onClick={upload}>
+                            Upload
+                        </Button>
+                    </Box>
+                </Paper>
+            )}
         </Container>
     );
 };
